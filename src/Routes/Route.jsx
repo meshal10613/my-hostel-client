0d6005f2f097705242461b9b6048fef5bbb91640
programming105 <meshal3didi@gmail.com>
2025-08-07 11:31:51 +0600

    some changes applied

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -2,8 +2,6 @@ import { createBrowserRouter } from "react-router";
 import HomeLayout from "../Layouts/HomeLayout";
 import Home from "../pages/Home";
 import ErrorPage from "../Components/Shared/ErrorPage";
-import Login from "../Pages/Authentication/Login";
-import Register from "../Pages/Authentication/Register";
 import AuthPage from "../Pages/Authentication/AuthPage";
 import Loading from "../Components/Shared/Loading";
 
