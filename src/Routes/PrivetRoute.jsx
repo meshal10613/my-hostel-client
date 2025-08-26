d76982126caee464fe8590646c283188b01eaa37
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-08-26 19:13:15 +0600

    privet route merged

--- src/Routes/PrivetRoute.jsx
+++ src/Routes/PrivetRoute.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import useAuthContext from '../Hooks/useAuthContext';
 import Loading from '../Components/Shared/Loading';
-import { Navigate } from 'react-router';
+import { Navigate, useLocation } from 'react-router';
 
 const PrivetRoute = ({children}) => {
     const { user, loading } = useAuthContext();
+    const location = useLocation();
+    console.log(location)
 
     if(loading){
         return <Loading/>;
